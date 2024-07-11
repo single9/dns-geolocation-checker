bbb19fd60644174470a68b4d721e31ce56f8d595
Duye Chen <k078264@gmail.com>
2024-07-11 14:21:57 +0800

    refactor: update default config file path in main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod ip_geo_checker;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let path = env::var("CONFIG_PATH").unwrap_or("./configs/domain.toml".to_string());
+    let path = env::var("CONFIG_PATH").unwrap_or("./configs/config.toml".to_string());
     let parser = ConfigParser::new_with_path(path);
     let config = parser.config();
     let res = IpGeoChecker::new(reqwest::Client::new())
